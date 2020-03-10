1721619bba4f5968b93904a795481a6b157e74e4
adamyarger <yargeradam@gmail.com>
2020-03-10 11:32:21 -0500

    create sch_5

---
+++ ch_5/compose.js
@@ -0,0 +1,64 @@
+function curry(fn) {
+  const arity = fn.length;
+
+  return function $curry(...args) {
+    // console.log('called', $curry, fn)
+
+    // this is where currying occurs, since where binding the single 
+    // param to the curry function and returning it.
+
+    // then next time the function is called it remembers the single param
+    if (args.length < arity) {
+      return $curry.bind(null, ...args);
+    }
+
+    return fn.call(null, ...args);
+  };
+}
+const compose = (...fns) => (...args) => fns.reduceRight((res, fn) => [fn.call(null, ...res)], args)[0];
+const reduce = curry((fn, zero, xs) => xs.reduce(fn, zero));
+const replace = curry((re, rpl, str) => str.replace(re, rpl));
+
+const toUpperCase = x => x.toUpperCase();
+const exclaim = x => `${x}!`;
+const shout = compose(exclaim, toUpperCase);
+
+// console.log(shout('send in the clowns'));
+
+// in functional programming we never use for loops
+// why?
+// beacause using a for loop means have have to use a mutatable vairable which is not a pure function.
+// what we use instead is highorder functions and recrusion
+
+// composing allows use to avoid nesting functions
+
+// how could you use compose for the document utils file
+
+const head = x => x[0];
+const reverse = reduce((acc, x) => [x].concat(acc), []);
+const last = compose(head, reverse);
+// console.log( last(['jumpkick', 'roundhouse', 'uppercut']) ); 
+
+const arg = ['jumpkick', 'roundhouse', 'uppercut'];
+const lastUpper = compose(toUpperCase, head, reverse);
+// const loudLastUpper = compose(exclaim, toUpperCase, head, reverse);
+
+const loudLastUpper = compose(exclaim, toUpperCase, last)
+// console.log(loudLastUpper(arg))
+
+
+// not pointfree because we mention the data: word
+const snakeCase_ = word => word.toLowerCase().replace(/\s+/ig, '_');
+// console.log( snakeCase_('Cool Man') )
+
+// pointfree
+const toLowerCase = x => x.toLowerCase()
+const snakeCase = compose(replace(/\s+/ig, '_'), toLowerCase);
+console.log( snakeCase('Cool My Dude') )
+
+
+
+
+
+
+
