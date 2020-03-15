ddd69e5e4c4fbedab534739ee500a92a590d8755
adamyarger <yargeradam@gmail.com>
2020-03-15 15:55:14 -0500

    stash

---
+++ ch_5/compose.js
@@ -0,0 +1,7 @@
+const compose = (f, g) => x => f(g(x))
+
+const toUpperCase = x => x.toUpperCase();
+const exclaim = x => x + '!';
+const shout = compose(toUpperCase, exclaim)
+
+console.log(shout('send in the clowns'))
