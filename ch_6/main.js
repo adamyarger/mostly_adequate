c62f5e68a33a5e0f342cf90d1896735049b93845
adamyarger <yargeradam@gmail.com>
2020-03-15 18:34:24 -0500

    sets up flickr app base

---
+++ ch_6/main.js
@@ -0,0 +1,12 @@
+const CDN = s => `https://cdnjs.cloudflare.com/ajax/libs/${s}`;
+const ramda = CDN('ramda/0.21.0/ramda.min');
+const jquery = CDN('jquery/3.0.0-rc1/jquery.min');
+
+requirejs.config({ paths: { ramda, jquery } });
+requirejs(['jquery', 'ramda'], ($, { compose, curry, map, prop }) => {
+  const Impure = {
+    getJSON: curry((callback, url) => $.getJSON(url, callback)),
+    setHtml: curry((sel, html) => $(sel).html(html)),
+    trace: curry((tag, x) => {console.log(tag, x); return x;})
+  }
+});
